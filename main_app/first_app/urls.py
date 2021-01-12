f9fcccffda88d514bb8e31e5ee551a591325c499
anasir514 <anasir514@gmail.com>
2021-01-12 16:49:20 +0000

    put in static folder

--- main_app/first_app/urls.py
+++ main_app/first_app/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from first_app import views
 
 urlpatterns = [
-    path('', views.todolist, name='todolist')
+    path('', views.todolist, name='todolist'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('pricing/', views.pricing, name='pricing')
 
 ]
 
--- main_app/first_app/views.py
+++ main_app/first_app/views.py
@@ -3,5 +3,25 @@ from django.http import HttpResponse
 
 # Create your views here.
 def todolist(request):
-    return render(request, 'todolist.html', {})
+    context = {
+        'welcome_text':"welcome from todolist."
+    }
+    return render(request, 'todolist.html', context)
 
+def about(request):
+    context = {
+        'welcome_text':"welcome from about."
+    }
+    return render(request, 'about.html', context)
+
+def contact(request):
+    context = {
+        'welcome_text':"welcome from contact."
+    }
+    return render(request, 'contact.html', context)
+
+def pricing(request):
+    context = {
+        'welcome_text':"welcome from pricing."
+    }
+    return render(request, 'pricing.html', context)
