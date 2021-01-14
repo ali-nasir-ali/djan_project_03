d542ff97dd554ecbdca95f24af152ec47331ce1b
anasir514 <anasir514@gmail.com>
2021-01-14 15:28:08 +0000

    added pendin funtionality

--- main_app/first_app/urls.py
+++ main_app/first_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from first_app import views
 
-urlpatterns = [
+urlpatterns = [ 
     path('', views.todolist, name='todolist'),
+    path('delete/<task_id>', views.delete_task, name='delete_task'),
+    path('complete/<task_id>', views.complete_task, name='complete_task'),
+    path('pending/<task_id>', views.pending_task, name='pending_task'),
+    path('edit/<task_id>', views.edit_task, name='edit_task'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('pricing/', views.pricing, name='pricing')
--- main_app/first_app/views.py
+++ main_app/first_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from first_app.models import TaskList
 from first_app.forms import TaskForm
+from django.contrib  import messages
  
 # Create your views here.
 def todolist(request):
@@ -9,10 +10,40 @@ def todolist(request):
         form = TaskForm(request.POST or None)
         if form.is_valid():
             form.save()
+        messages.success(request,("New Task Added"))    
         return redirect('todolist')   
     else: 
         all_tasks = TaskList.objects.all
         return render(request, 'todolist.html', {'all_tasks':all_tasks})
+
+def edit_task(request, task_id):
+    if request.method == "POST":
+        task =TaskList.objects.get(pk=task_id)
+        form = TaskForm(request.POST or None, instance= task )
+        if form.is_valid():
+            form.save()
+        messages.success(request,("Task Edited"))    
+        return redirect('todolist')   
+    else: 
+        task_obj = TaskList.objects.get(pk=task_id)
+        return render(request, 'edit.html', {'task_obj':task_obj}) 
+
+def pending_task(request,task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = False
+    task.save()
+    return redirect('todolist')
+
+def complete_task(request,task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = True
+    task.save()
+    return redirect('todolist')
+
+def delete_task(request,task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.delete()
+    return redirect('todolist')
 
 def about(request):
     context = {
