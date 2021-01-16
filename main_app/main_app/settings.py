e9a127f75c64fc53ed2d125b06de35415d68b981
anasir514 <anasir514@gmail.com>
2021-01-16 17:28:27 +0000

    text

--- main_app/main_app/settings.py
+++ main_app/main_app/settings.py
@@ -1,3 +1,4 @@
+import django-heroku
 import os
 import environ 
 
@@ -111,6 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "templates")]
 
@@ -119,3 +121,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = "todolist"
 LOGIN_URL = "login"
 
+django_heroku.settings(locals())
