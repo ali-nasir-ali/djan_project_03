3380a333be5b103bc6fc7aa61ddf6d2f9ad16ba0
anasir514 <anasir514@gmail.com>
2021-01-16 13:14:44 +0000

    put in decorators

--- main_app/first_app/views.py
+++ main_app/first_app/views.py
@@ -5,8 +5,9 @@ from first_app.models import TaskList
 from first_app.forms import TaskForm
 from django.contrib  import messages
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
  
-# Create your views here.
+@login_required
 def todolist(request):
     if request.method == "POST":
         form = TaskForm(request.POST or None)
@@ -21,6 +22,7 @@ def todolist(request):
         all_tasks = paginator.get_page(page)
     return render(request, 'todolist.html', {'all_tasks':all_tasks})
 
+@login_required
 def edit_task(request, task_id):
     if request.method == "POST":
         task =TaskList.objects.get(pk=task_id)
@@ -33,18 +35,21 @@ def edit_task(request, task_id):
         task_obj = TaskList.objects.get(pk=task_id)
         return render(request, 'edit.html', {'task_obj':task_obj}) 
 
+@login_required
 def pending_task(request,task_id):
     task = TaskList.objects.get(pk=task_id)
     task.done = False
     task.save()
     return redirect('todolist')
 
+@login_required
 def complete_task(request,task_id):
     task = TaskList.objects.get(pk=task_id)
     task.done = True
     task.save()
     return redirect('todolist')
 
+@login_required
 def delete_task(request,task_id):
     task = TaskList.objects.get(pk=task_id)
     task.delete()
@@ -68,6 +73,7 @@ def contact(request):
     }
     return render(request, 'contact.html', context)
 
+@login_required
 def pricing(request):
     context = {
         'welcome_text':"welcome from pricing."
